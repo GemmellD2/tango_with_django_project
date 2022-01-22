61446ae58704d9ffbd5a741ce38df72f39630e4f
David Gemmell <2559087G@student.gla.ac.uk>
2022-01-22 15:00:11 +0000

    Chapter 4 completed with Exercises

--- rango/views.py
+++ rango/views.py
@@ -13,4 +13,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html')
+    context_dict={'boldmessage': 'This tutorial has been put together by David Gemmell'}
+    return render(request, 'rango/about.html', context=context_dict)
